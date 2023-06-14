d35050990e75841d85279727a632f6a6e989ae40
wanggeng <18367319791@163.com>
2023-06-14 09:33:02 +0800

    feat:配置常用路径别名方便引入 安装@types/node,修改vite配置文件vite.config.ts设置常见路径别名。

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path"; // path用到了 @types/node 这个包所以要安装
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  //这里进行配置别名
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"), // @代替./src
+      "@api": path.resolve("./src/api"), // @api代替./src/api
+      "@components": path.resolve("./src/components"), // @components代替./src/components
+      "@views": path.resolve("./src/views"), // @views代替../src/views
+      "@utils": path.resolve("./src/utils"), // @utils代替./src/utils
+      "@types": path.resolve("./src/types"), // @types代替./src/types
+    },
+  },
+});
